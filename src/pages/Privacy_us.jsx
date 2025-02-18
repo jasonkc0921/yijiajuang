394b22194b6c6fc2fc5e8aaf00c97179a17bdc12
Jason Lin <jason.lin@exfo.com>
2025-02-18 22:59:57 +0800

    add privacy

--- src/pages/Privacy_us.jsx
+++ src/pages/Privacy_us.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Footer from "../components/Footer"
 import { Privacy } from "../components/Privacy"
 
-export const About_us = () => {
+export const Privacy = () => {
     return <div>
     <Privacy />
     {/* <Footer /> */}
